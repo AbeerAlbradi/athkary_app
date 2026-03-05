ba8712cc10a33bddce4514d1402301e8ca92f247
Abeer Albradi <abeeralbrdi@icloud.com>
2026-03-05 04:05:18 +0300

    final update

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'أذكاري',
-      theme: ThemeData(fontFamily: 'Cair
+      theme: ThemeData(fontFamily: 'Cairo'),
+      home: HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
